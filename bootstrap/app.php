34bba1ca4b72fc739cba7b18a87a661cf388f588
ThiagoZamprognioBahia <zcranigan11gmail.com>
2024-04-04 09:40:33 -0300

    Ajuste de nomenclatura

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
 
         $exceptions->reportable(function (InvalidIdException $e) {
-            info('Product Exception: ' . $e->getMessage());
+            info('Id Exception: ' . $e->getMessage());
         });
 
     })->create(); 
